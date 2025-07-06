33bcf08215d105562407e39ae1988bbdeedafbc7
yunaimatsu <yunaimatsu@users.noreply.github.com>
2025-07-06 21:07:38 +0900

    DELETE: Backup automation

--- backup.sh
+++
@@ -1,8 +0,0 @@
-branch_name="arch01"
-log_date="date '+%Y-%m-%d-%H-%M'"
-cd ~/yunaimatsu
-git add .
-git commit -m "$log_date"
-git push -u origin
-gh pr create --title "$log_date" --body ""
-echo "$(date +"%Y-%m-%d %H:%M:%S") Backup executed" >> .log
